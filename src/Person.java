7d6866edd0659e4d7ab999bcddc6a30ab78005f1
LAB12_USER ON WS15 <L12X09W15@LabHCI.net>
2025-11-28 10:21:52 +0800

    added Person

---
+++ src/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    private final String name;
+    private int age;
+    private String BirthDate;
+
+    public Person(String name, int age, String birthDate) {
+        this.name = name;
+        this.age = age;
+        BirthDate = birthDate;
+    }
+
+
+}
