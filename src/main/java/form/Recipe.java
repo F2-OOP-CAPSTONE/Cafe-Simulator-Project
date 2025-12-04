17e1fce2def69ae4d013f94c8027d8d83ddcd78f
kylle401 <plkylle83@gmail.com>
2025-12-04 20:48:09 +0800

    drink making demo, very cool, very nice

---
+++ src/main/java/form/Recipe.java
@@ -0,0 +1,18 @@
+package form;
+
+import javax.swing.*;
+
+public class Recipe extends JFrame{
+    private JPanel panel1;
+    private JPanel SigmaMode;
+    private JPanel BetaNerd;
+    private JPanel AlphaWolf;
+    private JPanel CoolGuySupreme;
+    private JPanel GerkinMyBerkin;
+
+    public Recipe(){
+        setContentPane(panel1);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        pack();
+    }
+}
